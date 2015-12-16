a329235b6bbe92bab8be6399a47305ce8257aa22
aaronchun7 <aaronchun7@gmail.com>
2015-12-16 15:51:40 -0600

    Set up method stubs for all methods besides tests

---
+++ calculator/src/calculator/Calculator.java
@@ -0,0 +1,88 @@
+package calculator;
+
+import java.util.List;
+
+/**
+ * 
+ * @maxcharperline 70
+ * @author Jimmy Kuczynski - kuczynskij
+ * @author Aaron Chun - chunaa
+ */
+public class Calculator {
+	
+	public static void main(String [] args){
+		
+	}
+	
+	/**
+	 * User Story #1
+	 * @author kuczynskij
+	 * @param l - list of integers
+	 * @return
+	 */
+	public int add(List<Integer> l){
+		return 0;
+	}
+	
+	/**
+	 * User Story #2
+	 * @author chuna
+	 * @return
+	 */
+	public int sub(List<Integer> l){
+		return 0;
+	}
+	
+	/**
+	 * User Story #3
+	 * @author kuczynskij
+	 * @return
+	 */
+	public int mult(List<Integer> l){
+		return 0;
+	}
+	
+	/**
+	 * User Story #4
+	 * @author chuna
+	 * @return
+	 */
+	public int div(List<Integer> l){
+		return 0;
+	}
+	
+	/**
+	 * User Story #5
+	 * @author kuczynskij
+	 * @param num	Result of the 
+	 * @return
+	 */
+	public int hist(int num){
+		return 0;
+	}
+	
+	/**
+	 * User Story #6
+	 * @author chuna
+	 * @return
+	 */
+	public boolean clear(){
+		return false;
+	}
+	
+	/**
+	 * User Story #7
+	 * @author kuczynskij
+	 */
+	public void reuse(){
+		
+	}
+	
+	/**
+	 * User Story #8
+	 * @author chuna
+	 */
+	public void wombo(){
+		
+	}
+}
